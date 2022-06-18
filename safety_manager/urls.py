0e7c53466ef5ccd9717166d1b9196ef07d09112a
dm0114 <dm10802@gmail.com>
2022-06-18 19:13:17 +0900

    plz..

--- safety_manager/urls.py
+++ safety_manager/urls.py
@@ -1,9 +1,10 @@
 from django.conf import settings
 from django.contrib import admin
 from django.urls import path, include
-from django.conf.urls.static import static
+from . import views
 
 urlpatterns = [
+    path('', views.index),
     path('admin/', admin.site.urls),
     path('employee/', include('employee.urls')),
     path('manager/', include('manager.urls')),
@@ -11,7 +12,3 @@ urlpatterns = [
     path('suggestions/', include('suggestions.urls')),
     path('complain/', include('complain.urls'))
 ]
-
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL,
-                        document_root=settings.MEDIA_ROOT)
